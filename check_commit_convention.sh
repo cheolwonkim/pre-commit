8fefd281199caf00905d845717b92579b3b2912f
cheolwon kim <yohan1422@gmail.com>
2024-05-14 15:50:45 +0900

    Update check_commit_convention.sh

--- check_commit_convention.sh
+++ check_commit_convention.sh
@@ -8,8 +8,8 @@ fi
 
 COMMIT_MSG_FILE="$1"
 
-# Regular expression for Conventional Commits
-CONVENTIONAL_REGEX="^(fix|feat|perf|chore|docs|style|revert|refactor|break|test|ci|bump|Bump version|hotfix|ci-skip|bump-skip)(\([a-z ]+\))?: [\w ]+$"
+# Improved regular expression for Conventional Commits
+CONVENTIONAL_REGEX="^(fix|feat|perf|chore|docs|style|revert|refactor|break|test|ci|bump|Bump version|hotfix|ci-skip|bump-skip)(\([a-z ]+\))?: .+$"
 
 # Check if the commit message matches the conventional commits pattern
 if ! grep -qE "$CONVENTIONAL_REGEX" "$COMMIT_MSG_FILE"; then
@@ -17,6 +17,23 @@ if ! grep -qE "$CONVENTIONAL_REGEX" "$COMMIT_MSG_FILE"; then
     echo "
 Your commit message doesn't conform to Conventional Commit Rules (https://www.conventionalcommits.org/).
 At a minimum, your commit should start with one of the following types:
+
+fix: Correct a bug causing an error
+feat: Add a new feature
+perf: Improve performance
+chore: Update build tasks
+docs: Update documentation
+style: Update code style
+revert: Revert a change
+refactor: Refactor code
+break: Introduce breaking changes
+test: Add or update tests
+ci: Update CI configuration
+bump: Update dependencies
+Bump version: Bump the version
+hotfix: Apply a hotfix
+ci-skip: Skip CI
+bump-skip: Skip dependency bump
 
 Example commit message to add a feature: 'feat: Add a new feature'
 Example commit message to fix an issue: 'fix: Correct a bug causing an error'
